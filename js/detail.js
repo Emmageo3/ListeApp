b79548e933b583847f0d2a934f42752c74ac98e8
Emmageo3 <kanfanyemma22@gmail.com>
2021-12-29 16:13:54 +0000

    le design est moche

--- js/detail.js
+++ js/detail.js
@@ -36,19 +36,15 @@ function afficherDetail(idee){
     detail.insertAdjacentHTML(
         "beforeend",
         `
-        <div class="row">
-                    <a href="liste.html"><img src="./img/arrow.png" height= "25rem" width="35rem"></a>
-        </div>
         <div class="col-5 mt-1" style = "height: 19rem; " >
             <div class="row">
                 <div class="col-10">
-                    <img src="" alt="" id="img" style="box-shadow: 10px 12px 29px -6px #000000;border-radius: 10px">
+                    <img src="" alt="" id="img" style="border-radius: 10px">
                 </div>
-
             </div>
         
         </div>
-        <div class="col-7 mt-1" style=" height: 19rem; box-shadow: 10px 12px 29px -6px #000000; border-radius: 10px">
+        <div class="col-7 mt-1" style=" height: 19rem; border-radius: 10px">
             <div class="row">
                 <h1 id="prenom">${idee.prenom +" "+ idee.nom}</h1>
             </div>
@@ -178,12 +174,3 @@ window.addEventListener("DOMContentLoaded", (event) => {
         })
     })
 })
-
-
-
-
-
-
-
-
-
---
+++ js/test.js
@@ -0,0 +1,26 @@
+    
+let countDate = new Date('Dec 31, 2021');
+function countDown(){
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / day);
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    countDown()
+},1000)
